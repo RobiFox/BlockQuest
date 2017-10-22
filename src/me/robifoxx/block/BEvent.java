536bebfa77deab698c9e21fcdc9ec4982deffebb
unknown <RobiFox@users.noreply.github.com>
2017-10-22 21:16:16 +0200

    reverted code that was thought to be useless

--- src/me/robifoxx/block/BEvent.java
+++ src/me/robifoxx/block/BEvent.java
@@ -215,9 +215,23 @@ public class BEvent implements Listener {
                                             .replace("%blockLeft%", "" + blocksLeft)
                                             .replace("%blocksLeft%", "" + blocksLeft));
                                 }
+                            } else {
+                                for(String s : plugin.getConfig().getStringList("already-found-commands")) {
+                                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), s.replace("%player%", e.getPlayer().getName())
+                                            .replace("%pLocX%", "" + e.getPlayer().getLocation().getX())
+                                            .replace("%pLocY%", "" + e.getPlayer().getLocation().getY())
+                                            .replace("%pLocZ%", "" + e.getPlayer().getLocation().getZ())
+                                            .replace("%locX5%", "" + (e.getClickedBlock().getLocation().getX() + 0.5))
+                                            .replace("%locY5%", "" + (e.getClickedBlock().getLocation().getY() + 0.5))
+                                            .replace("%locZ5%", "" + (e.getClickedBlock().getLocation().getZ() + 0.5))
+                                            .replace("%locX%", "" + e.getClickedBlock().getLocation().getX())
+                                            .replace("%locY%", "" + e.getClickedBlock().getLocation().getY())
+                                            .replace("%locZ%", "" + e.getClickedBlock().getLocation().getZ())
+                                            .replace("%blockLeft%", "" + blocksLeft)
+                                            .replace("%blocksLeft%", "" + blocksLeft));
+                                }
                             }
-                            /*int blocksLeft = plugin.getConfig().getStringList("blocks").size() - Main.blocksss.get(e.getPlayer().getName()).size();
-                            if(blocksLeft <= 0) {
+                            /*if(blocksLeft <= 0) {
                                 for(String s : plugin.getConfig().getStringList("already-found-all-blocks")) {
                                     Bukkit.dispatchCommand(Bukkit.getConsoleSender(), s.replace("%player%", e.getPlayer().getName())
                                             .replace("%pLocX%", "" + e.getPlayer().getLocation().getX())
