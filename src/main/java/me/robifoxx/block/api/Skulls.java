824f62773eb5e41543faa204b018aa51643845d3
RobiFox <10639273+RobiFox@users.noreply.github.com>
2019-08-02 13:13:46 +0200

    version compatibility
    ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss

--- src/main/java/me/robifoxx/block/api/Skulls.java
+++ src/main/java/me/robifoxx/block/api/Skulls.java
@@ -2,6 +2,7 @@ package me.robifoxx.block.api;
 
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -23,7 +24,14 @@ public class Skulls {
     public static ItemStack createSkull(String urlToFormat) {
         String s = urlToFormat.replace("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUv", "");
         String url = "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUv" + s;
-        ItemStack head = new ItemStack(Material.PLAYER_HEAD);
+
+        int ver = Integer.parseInt(Bukkit.getBukkitVersion().split("\\.")[1]); // Will be changed in 2.0
+        ItemStack head;
+        if(ver > 12) {
+            head = new ItemStack(Material.PLAYER_HEAD);
+        } else {
+            head = new ItemStack(Material.valueOf("SKULL_ITEM"), 1, (byte) 3);
+        }
         if (url.isEmpty()) return head;
 
         SkullMeta headMeta = (SkullMeta) head.getItemMeta();
