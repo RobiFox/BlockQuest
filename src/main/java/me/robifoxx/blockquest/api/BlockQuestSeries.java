925b0fd67451d55bfd22ca5a5583312e9397ba00
RobiFox <10639273+RobiFox@users.noreply.github.com>
2019-09-28 17:00:19 +0200

    documentation

--- src/main/java/me/robifoxx/blockquest/api/BlockQuestSeries.java
+++ src/main/java/me/robifoxx/blockquest/api/BlockQuestSeries.java
@@ -3,18 +3,70 @@ package me.robifoxx.blockquest.api;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
+/**
+ * A BlockQuest Series consists of
+ * hidden blocks.
+ */
 public abstract class BlockQuestSeries {
+    /**
+     * The ID of the Block Quest Series.
+     * @return Return
+     */
     public abstract String getID();
-    public abstract boolean isEnabled();
-    public abstract int getRequiredInventorySpace();
 
+    /**
+     * Whether the Series is enabled or not
+     * @return Return
+     */
+    public abstract boolean isEnabled();
+
+    /**
+     * Action that happens, when a player finds a block.
+     * @param p The player, that clicked the block.
+     * @param blockLocation The location of the block
+     */
     public abstract void getFindBlockAction(Player p, Location blockLocation);
+
+    /**
+     * Action that happens, when a player finds the last block of the series.
+     * @param p The player, that clicked the block.
+     * @param blockLocation The location of the block
+     */
     public abstract void getFoundAllBlockAction(Player p, Location blockLocation);
+
+    /**
+     * Action that happens, when a player clicks a block that the player has already found.
+     * @param p The player, that clicked the block.
+     * @param blockLocation The location of the block
+     */
     public abstract void getAlreadyFoundBlockAction(Player p, Location blockLocation);
+
+    /**
+     * Action that happens, when a right clicks a block in a series, when he already found all blocks.
+     * @param p The player, that clicked the block.
+     * @param blockLocation The location of the block
+     */
     public abstract void getAlreadyFoundAllBlockAction(Player p, Location blockLocation);
 
-    public abstract void playNotFoundParticle(Player p, Location blockLocation);
-    public abstract void playFoundParticle(Player p, Location blockLocation);
+    /**
+     * Action, that plays for the player every {@link #getRepeatTime()} ticks for blocks, that are not found in the series.
+     * @param p The player, that hasn't found the block.
+     * @param blockLocation The location of the block
+     */
+    public abstract void playNotFoundEffect(Player p, Location blockLocation);
+
+    /**
+     * Action, that plays for the player every {@link #getRepeatTime()} ticks for blocks, that are found in the series.
+     * @param p The player, that has found the block.
+     * @param blockLocation The location of the block
+     */
+    public abstract void playFoundEffect(Player p, Location blockLocation);
+
+    /**
+     * The frequency of the block indicator, that shows whether the player found the block or not.
+     * @return Return
+     */
+    public abstract int getRepeatTime();
 
     // TODO find effect
 }
